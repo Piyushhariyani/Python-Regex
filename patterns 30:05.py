d3e89551dbaecca3cbda6b08d34a1056ba45741e
Piyush hariyani <hapiyush16@gmail.com>
2025-05-30 01:35:53 +0530

    Add files via upload

---
+++ patterns 30:05.py
@@ -0,0 +1,212 @@
+for i in range (1,5):
+    for j in range(1,5-i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,5-i+1):
+        print(j,end='')
+    print()
+
+print()
+
+for i in range(1,4):
+    x=66
+    for j in range(1,4-i+1):
+        print(x,end='')
+        x+=1
+    print()
+
+print()
+
+for i in range(1,5):
+    ch=65
+    for j in range(1,i+1):
+        print(chr(ch),end='')
+        ch+=1
+    print()
+
+print()
+
+x=1
+for i in range(1,5):
+    for j in range(1,5-i+1):
+        print(x,end='')
+        x+=1
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print("?",end='')
+    for k in range(1,5-i):
+        print(k,end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,5-i+1):
+        print(j,end='')
+    for k in range(1,i):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
+
+print()
+
+x=1
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(x,end='')
+        x+=1
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,6-i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    ch=65
+    for j in range(1,5-i+1):
+        print(chr(ch),end='')
+        ch+=1
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(i,end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(5,i,-1):
+        print(j-1,end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    ch=65
+    for j in range(1,i+1):
+        print(chr(ch),end='')
+        ch+=1
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,6-i+1):
+        print(j,end='')
+    print()
+
+print()
+
+for i in range(1,7):
+    ch=70
+    for j in range(1,7-i+1):
+        print(chr(ch),end='')
+        ch-=1
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,i):
+        print(" ",end='')
+    for k in range(1,6-i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(" ",end='')
+    for k in range(1,i+1):
+        print("*",end='')
+    print()
+
+print()
+
+x=1
+ch=97
+for i in range(1,5):
+    for j in range(1,i*2+1):
+        if(j%2==0):
+            print(chr(ch),end='')
+            ch+=1
+        else:
+            print(x,end='')
+            x+=1
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,5):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(0,4):
+    for j in range(0,i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,5-i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,i):
+        print(" ",end='')
+    for k in range(1,5-i+1):
+        print("*",end=' ')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,5-i):
+        print(" ",end='')
+    for k in range(1,i+1):
+        print("*",end=' ')
+    print()
