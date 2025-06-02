0915d9661f8e721594c74cf812f74b8365708402
Piyush hariyani <hapiyush16@gmail.com>
2025-06-03 00:01:18 +0530

    Add files via upload

---
+++ patterns1:6.py
@@ -0,0 +1,62 @@
+for i in range(1,5):
+    for j in range(1,i):
+        print(" ",end='')
+    for k in range(1,5):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,5):
+    for j in range(1,i):
+        print("-",end='')
+    for k in range(1,5-i):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(" ",end='')
+    for k in range(1,i+1):
+        print("*",end=' ')
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,i):
+        print(" ",end='')
+    for k in range(1,6-i+1):
+        print("*",end=' ')
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,6-i+1):
+        print("*",end='')
+    for k in range(1,i+1):
+        print(" ",end='')
+    print()
+for i in range(2,6):
+    for j in range(1,i+1):
+        print("*",end='')
+    print()
+
+print()
+
+for i in range(1,6):
+    for j in range(1,i):
+        print(" ",end='')
+    for k in range(1,6-i+1):
+        print("*",end='')
+    print()
+for i in range(2,6):
+    for j in range(1,6-i):
+        print(" ",end='')
+    for k in range(1,i+1):
+        print("*",end='')
+    print()
+
